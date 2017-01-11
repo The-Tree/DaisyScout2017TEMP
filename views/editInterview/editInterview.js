1f555fd2aff3734f24059fb65461c544d04d5829
Kenny Daily <aves.lacertilia@gmail.com>
2017-01-10 19:50:27 -0500

    We continue to test the gitshell

--- views/editInterview/editInterview.js
+++ views/editInterview/editInterview.js
@@ -77,6 +77,7 @@ angular.module('daisyscoutApp.editInterviewRecord', ['ngRoute'])
 				//NOT TO DO: redirect to main page
 				
 				//this is a test
+				//this is a second test
             });
 		}
 	}
